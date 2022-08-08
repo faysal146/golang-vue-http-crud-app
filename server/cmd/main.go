abe8a4a3ea090d8e1800f10b3773be70343e8530
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-08-08 21:30:45 +0600

    feat: :tada: setup app structure and load env
    setup db Initializer and server Initializer and load environment variables
    
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "github.com/faysal146/golang-vue-http-crud-app/server/server"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var envKeys = []string{"DB_USER_NAME", "DB_PASSWORD", "DB_NAME", "PORT"}
+
+func init() {
+	err := godotenv.Load("../env/config.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	for _, key := range envKeys {
+		if _, ok := os.LookupEnv(key); !ok {
+			fmt.Printf("%v env not loaded\n", key)
+			os.Exit(1)
+		}
+	}
+	fmt.Println("environment variables loaded successfully")
+}
 
 func main() {
-	server.StartServer()
+	app := App{}
+	app.Initialize(
+		os.Getenv("DB_USER_NAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+	app.Run(fmt.Sprintf(":%v", os.Getenv("PORT")))
 }
