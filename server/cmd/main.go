ed82b2f8809023323e1aa87ff9e02279c59fd3c3
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-08-09 20:29:24 +0600

    revert: :ambulance: Initialize database and start server
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,20 +4,23 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/faysal146/golang-vue-http-crud-app/server/database"
 	"github.com/faysal146/golang-vue-http-crud-app/server/pkg/app"
 	"github.com/faysal146/golang-vue-http-crud-app/server/util"
 )
 
 func init() {
+	// load environment variables
 	util.LoadEnv(util.EnvKeys)
 }
 
 func main() {
-	a := app.App{}
-	a.Initialize(
+	// connect database
+	database.InitializeDB(
 		os.Getenv("DB_USER_NAME"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
-	a.Run(fmt.Sprintf(":%v", os.Getenv("PORT")))
+	// start server
+	app.Run(fmt.Sprintf(":%v", os.Getenv("PORT")))
 }
