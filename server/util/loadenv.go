78ffc07e03d2957990272a3b33712dc56bb14d7e
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-08-09 14:41:09 +0600

    refactor: :art: move env loader on separate package
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

---
+++ server/util/loadenv.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	"github.com/joho/godotenv"
+)
+
+var EnvKeys = []string{"DB_USER_NAME", "DB_PASSWORD", "DB_NAME", "PORT"}
+
+func LoadEnv(envKeys []string) {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		fmt.Println("unable to get the current filename")
+		os.Exit(1)
+	}
+	dirname := filepath.Dir(filename)
+	var err error
+	if os.Getenv("GO_ENV") == "test" {
+		err = godotenv.Load(
+			filepath.Join(dirname, "../", "env/test.env"),
+		)
+	} else {
+		err = godotenv.Load(filepath.Join(dirname, "../env/config.env"))
+	}
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+	for _, key := range envKeys {
+		if _, ok := os.LookupEnv(key); !ok {
+			fmt.Printf("%v env not loaded\n", key)
+			os.Exit(1)
+		}
+	}
+	fmt.Println("environment variables loaded successfully")
+}
