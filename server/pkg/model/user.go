795d3c9d84215f6d770a781859616fbc7232dd04
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-08-09 18:58:45 +0600

    fix: :bug: remove read and write gorm tag
    remove gorm read and write tag from user struct
    
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- server/pkg/model/user.go
+++ server/pkg/model/user.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"fmt"
 	"time"
 
 	"gorm.io/gorm"
@@ -9,13 +8,13 @@ import (
 
 type User struct {
 	// autoCreateTime autoUpdateTime check:age > 13
-	ID                string    `json:"id" gorm:"primaryKey;default:uuid_generate_v3(); not null"`
+	ID                string    `json:"id" gorm:"primaryKey;default:uuid_generate_v4()"`
 	FirstName         string    `json:"first_name"`
 	LastName          string    `json:"last_name"`
 	Username          string    `json:"user_name"`
 	Email             string    `json:"email" gorm:"unique"`
-	Role              string    `json:"role" gorm:"<-:false;default:user"`
-	Password          string    `json:"password" gorm:"->:false"`
+	Role              string    `json:"role" gorm:"default:user"`
+	Password          string    `json:"password"`
 	PasswordUpdatedAt time.Time `json:"password_updated_at"`
 	Token             string    `json:"token"`
 	RefreshToken      string    `json:"refresh_token"`
@@ -25,15 +24,4 @@ type User struct {
 
 func MigrateUser(db *gorm.DB) error {
 	return db.AutoMigrate(&User{})
-}
-
-func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
-	if u.Email == "admin@example.com" {
-		u.Role = "admin"
-	}
-	return
-}
-func (u *User) AfterCreate(tx *gorm.DB) (err error) {
-	fmt.Println("user created. user id is :", u.ID)
-	return
 }
