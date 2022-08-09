610e72a3f9ee1e8114ee575c13656a262c2d4348
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-08-10 02:37:02 +0600

    feat: :building_construction: custom error message from validation errors
    get human friendly custom error message
    
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

---
+++ server/pkg/helpers/helpers.go
@@ -0,0 +1,32 @@
+package helpers
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func HashPassword(p string) string {
+	return ""
+}
+
+func VerifyPassword(hashPassword, textPassword string) bool {
+	return false
+}
+
+func GetValidationMessage(fe validator.FieldError) (key string, msg string) {
+	switch fe.Tag() {
+	case "required":
+		return fe.Field(), "is required"
+	case "email":
+		return fe.Field(), "Invalid email"
+	case "max":
+		return fe.Field(), fmt.Sprintf("maximum length (%v) exited", fe.Param())
+	case "min":
+		return fe.Field(), fmt.Sprintf("too short minimum length (%v)", fe.Param())
+	case "alpha":
+		return fe.Field(), "invalid value only alpha value accepted"
+	default:
+		return "error", fe.Error() // default error
+	}
+}
