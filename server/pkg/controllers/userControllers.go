a10c9aa93eb4a77ab53b63635b0b5ad602c9ed71
Faysal Ahmed <faysalahmed.dev@gmail.com>
2022-08-11 16:55:15 +0600

    feat: :sparkles: build user login logic
    add login api
    
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- server/pkg/controllers/userControllers.go
+++ server/pkg/controllers/userControllers.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -28,10 +27,6 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	if validationErr != nil {
 		// custom error messages customValidationErr{ email: "invalid email", .... }
 		var customValidationErr = make(map[string]string)
-		if _, ok := validationErr.(*validator.InvalidValidationError); ok {
-			fmt.Println(validationErr)
-			return
-		}
 		// range over all errors
 		for _, err := range validationErr.(validator.ValidationErrors) {
 			// get human friendly error message -> "email", "invalid email address"
@@ -48,10 +43,10 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 				if user not exist save user data to the database create jwt and refresh token
 		*/
 
-		var userExist model.User
+		var userExist bool
 		database.DBClient.Model(&model.User{}).Where("email = ?", strings.ToLower(bodyData.Email)).Or("username = ?", bodyData.Username).Find(&userExist)
 
-		if strings.TrimSpace(userExist.ID) != "" {
+		if !userExist {
 			helpers.RespondWithError(w, http.StatusBadRequest, "email or username already exist")
 		} else {
 			var userData = model.User{
@@ -69,6 +64,60 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 			userData.RefreshToken = refreshToken
 			database.DBClient.Model(&userData).Updates(model.User{Token: authToken, RefreshToken: refreshToken}).Where("id = ?", userData.ID)
 			helpers.RespondWithJSON(w, http.StatusCreated, userData)
+		}
+	}
+}
+
+func LoginUser(w http.ResponseWriter, r *http.Request) {
+	// check content type
+	headerContentTtype := r.Header.Get("Content-Type")
+	if headerContentTtype != "application/json" {
+		// response error
+		helpers.RespondWithError(w, http.StatusUnsupportedMediaType, "Content Type is not application/json")
+		return
+	}
+	// login either username or email and password
+	var loginBody model.LoginBody
+	// decode body data
+	json.NewDecoder(r.Body).Decode(&loginBody)
+	// validate email and password
+	validationErr := validator.New().Struct(loginBody)
+	if validationErr != nil {
+		// custom error messages customValidationErr{ email: "invalid email", .... }
+		var customValidationErr = make(map[string]string)
+		// range over all errors
+		for _, err := range validationErr.(validator.ValidationErrors) {
+			// get human friendly error message -> "email", "invalid email address"
+			key, msg := helpers.GetValidationMessage(err)
+			customValidationErr[key] = msg
+		}
+		// response back with error message
+		helpers.RespondWithError(w, http.StatusBadRequest, customValidationErr)
+	} else {
+		var userdata model.User
+		// file format correct
+
+		// check is user exist
+		database.DBClient.Model(&model.User{}).Where("email = ?", strings.ToLower(loginBody.Email)).Find(&userdata)
+		if userdata.Email == strings.ToLower(loginBody.Email) && strings.TrimSpace(userdata.ID) != "" {
+			// user found
+
+			// check password
+			if userdata.VerifyPassword(loginBody.Password) {
+				// password match
+				authToken, refreshToken := helpers.GenerateAuthToken(userdata.ID, userdata.Email)
+				// update token on database
+				database.DBClient.Model(&userdata).Updates(model.User{Token: authToken, RefreshToken: refreshToken}).Where("id = ?", userdata.ID)
+				userdata.Token = authToken
+				userdata.RefreshToken = refreshToken
+				helpers.RespondWithJSON(w, http.StatusOK, userdata)
+			} else {
+				// password don't match
+				helpers.RespondWithError(w, http.StatusBadRequest, "incorrect password")
+			}
+		} else {
+			// user not found
+			helpers.RespondWithError(w, http.StatusForbidden, "user not found")
 		}
 	}
 }
